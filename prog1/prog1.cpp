38fa2bc40271383d22cb6003b141a7416a71f19c
守真 孫 <oscarsun72@hotmail.com>
2019-08-10 19:23:39 +0800

    Add project files.

---
+++ prog1/prog1.cpp
@@ -0,0 +1,50 @@
+﻿// prog1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include<vector>
+//#include<iterator>
+using namespace std;
+//using std::cout; using std::cin;using std::endl;
+
+string compareStrEqual() {
+
+	vector<int>ivec{2,3,4,5,6,7};	
+	int iarr[6];
+	int* p = iarr;
+	for (vector<int>::iterator i = begin(ivec); i != end(ivec); ++i)
+	{
+		*p = *i; ++p;
+	}
+	for (auto i : iarr)
+	{
+		cout<<i<<" ";
+	}
+	cout << endl;
+	return "";
+}
+
+void test() {
+	cout << compareStrEqual() << endl;
+}
+
+int main() {
+	test();
+	return 0;
+}
+
+
+			//std::cout << "Hello World!\n";
+
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
