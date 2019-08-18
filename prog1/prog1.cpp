8022808c8fc3b2d254c444267cacdb0409eb7c8a
守真 孫 <oscarsun72@hotmail.com>
2019-08-18 13:12:32 +0800

    1

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -9,16 +9,20 @@ using namespace std;
 //using std::cout; using std::cin;using std::endl;
 
 void test() {	
-	string rsp;
-	do {
-		cout << "please enter two values: ";
-		int val1 = 0, val2 = 0;
-		cin >> val1 >> val2;
-		cout << "The sum of " << val1 << " and " << val2
-			<< " = " << val1 + val2 << "\n\n"
-			<< "More? Enter yes or no: ";
-		cin >> rsp;
-	} while (!rsp.empty() && rsp[0] != 'n');
+	(a) do
+		int v1, v2;
+	cout << "Please enter two numbers to sum:";
+	if (cin >> v1 >> v2)
+		cout << "Sum is: " << v1 + v2 << endl;
+	while (cin);
+	Click here to view code image
+	(b) do {
+		// . . .
+	} while (int ival = get_response());
+	Click here to view code image
+	(c) do {
+		int ival = get_response();
+	} while (ival);
 }
 	
 int main() {
