4ec68cd87345a743e2ded65ef1023a6ab201dcdf
守真 孫 <oscarsun72@hotmail.com>
2019-09-02 11:44:08 +0800

    1

--- prog1/Sales_data.cpp
+++ prog1/Sales_data.cpp
@@ -11,18 +11,18 @@ Sales_data& Sales_data::combine(const Sales_data& sales_data1,  const Sales_data
 }
 Sales_data& Sales_data::combine(const Sales_data& sales_data)
 {
-	if (bookNo == "") bookNo = sales_data.bookNo;//因為這是Sales_data類別的成員函式，所以可以直接通用該類別內的所有成員名稱，不必全名稱
+	if (bookNo == "") bookNo = sales_data.bookNo;//因為這是Sales_data類別的成員函式，所以可以直接調用該類別內的所有成員名稱，不必全名稱
 	bookSize = sales_data.bookSize;
 	soldQ += sales_data.soldQ;
 	revenue += sales_data.revenue;
 	return *this;// TODO: insert return statement here
 }
-string Sales_data::isbn(const Sales_data& sales_data)
+string Sales_data::isbn()const
 {
-	return sales_data.bookNo;
+	return bookNo;
 }
 
-double Sales_data::avg_price(const Sales_data& sales_data)
+double Sales_data::avg_price(const Sales_data& sales_data)const
 {
 	if (sales_data.soldQ>0)
 	{
--- prog1/Sales_data.h
+++ prog1/Sales_data.h
@@ -2,15 +2,16 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 #include<string>
+using namespace std;//千萬不要忘了這個
 struct  Sales_data {
-	std::string bookNo;
+	string bookNo;
 	double revenue{ 0.00 };//總營收-營業額
 	unsigned soldQ{ 0 };
 	double bookSize{0.00};
 	Sales_data& combine(const Sales_data&, const Sales_data&);//成員函式宣告一定要在類別內，定義可在此外
 	Sales_data& combine(const Sales_data&);//成員函式宣告一定要在類別內，定義可在此外
-	string isbn(const Sales_data&);
-	double avg_price(const Sales_data&);
+	string isbn()const;	
+	double avg_price(const Sales_data&)const;
 };
 #endif // !SALES_DATA_H
 
