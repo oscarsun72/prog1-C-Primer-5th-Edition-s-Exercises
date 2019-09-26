0452e9f780199bc7d6ae944febb8c11d054f6536
守真 孫 <oscarsun72@hotmail.com>
2019-09-26 21:43:37 +0800

    OK

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -3,33 +3,15 @@
 
 //using std::cout; using std::cin;using std::endl;
 //#include<cassert>//前置處理器（preprocessor）偵錯、斷言（assert）
-#include <iostream>
-#include <fstream>
-#include<sstream>
-#include<string>
-#include<vector>
+#include<deque>
+#include<list>
 using namespace std;
 
-struct PersonInfo {
-	string name;
-	vector<string>phones;
-};
-int main() {
-	string line, word; // will hold a line and word from input, respectively
-	vector<PersonInfo> people; // will hold all the records from the input
-	// read the input a line at a time until cin hits end-of-file (or another error)
-	istringstream record; // bind record to the line we just read
-	ifstream ifstrm("V:\\Programming\\C++\\new 2.h");//檔案編碼目前只支援 ANSI和Big5，還不支援UTF-8
-	while (getline(ifstrm, line)) {
-		PersonInfo info; // create an object to hold this record's data
-		record.str(line);
-		if (record.eof())//關鍵在這！因為record移位（shift）後在處理下一筆記錄（下一行）前未歸位
-			record.clear();
-		record >> info.name; // read the name
-		while (record >> word) // read the phone numbers
-			info.phones.push_back(word); // and store them
-		people.push_back(info); // append this record to people
-	}
+int main() {	
+	list<deque<int>> lstdqi;
+	//用別名來定義
+	typedef deque<int> dqi;
+	list<dqi> lstdqi_alias;	
 }
 
 
