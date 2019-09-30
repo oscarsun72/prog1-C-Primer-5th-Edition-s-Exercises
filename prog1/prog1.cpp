43b8571d71b30202cba965f24c5551b9d9a50c19
守真 孫 <oscarsun72@hotmail.com>
2019-09-30 14:20:41 +0800

    OK

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -4,26 +4,14 @@
 //using std::cout; using std::cin;using std::endl;
 //#include<cassert>//前置處理器（preprocessor）偵錯、斷言（assert）
 #include<vector>
+#include<list>
 using namespace std;
 
 int main() {
-	vector<int> veci;
-	//Every container type defines a default constructor (§ 7.1.4, p. 263). 
-	//With the 	exception of array, the default constructor creates an empty container of the	specified type.
-	/*Again excepting array, the other constructors take arguments that
-		specify the size of the container and initial values for the elements.*/
-	vector<int>veci1(3,2);
-	vector<int>veci2(3);
-	/*There are two ways to create a new container as a copy of another one: 
-	We can directly copy the container:*/
-	vector<int>veci3= veci1;//容器拷貝時，容器型別與元素型別必須一致
-	vector<int>veci31(veci1);//the constructor that takes a container to copy
-	// or (excepting array) we can copy a range of elements denoted by a pair of iterators.:
-	vector<int>veci4(veci2.begin(), veci2.end());
-	vector<int>veci5 = {1,1,1};
-	vector<int>veci6(4,4);
-	veci6 = { 1,1,1 };//size會改變成串列的，而不是不足的用值初始化！veci6.size=3,veci6.capacity()=4;
-	//好像只有array因為是固定大小，串列初始化（list initialization）不夠的剩下才會用值初始化
+	vector<int> veci{-1,-2,-3,0};
+	list<int> lsti{0,1,2,3};
+	vector<double> vecdi(veci.cbegin(),veci.cend());
+	vector<double> vecdl(lsti.cbegin(), lsti.cend());	
 }
 
 
