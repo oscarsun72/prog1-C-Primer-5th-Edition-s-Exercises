20f05dd919f5f9d94cad01ee783b38deb5b99b60
守真 孫 <oscarsun72@hotmail.com>
2019-11-24 18:23:06 +0800

    not done!

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -6,13 +6,28 @@
 
 #include<iostream>
 using namespace std;
-
-int main() {
-	auto sp = make_shared<int>(1);
-	auto p = sp.get();
-	sp.reset(p);
-	//delete p;//此行當改用上式來釋放p
+struct destination;	// represents what we are connecting to
+struct connection;           // information needed to use the connection
+connection connect(destination*); // open the connection
+void disconnect(connection);       // close the given connection
+int main() {	
 }
+void end_connection(connection* p) { disconnect(*p); }
+void f(destination & d /* other parameters */)
+	{
+		// get a connection; must remember to close it when done
+		connection c = connect(&d);
+		// use the connection
+		// if we forget to call disconnect before exiting f, there will be no way to close c
+	}
+void f(destination& d /* other parameters */)
+{
+	connection c = connect(&d);
+	shared_ptr<connection> p(&c, end_connection);
+	// use the connection
+	// when f exits, even if by an exception, the connection will be properly closed
+}
+
 
 
 //int main(int argc, const char** argv)
