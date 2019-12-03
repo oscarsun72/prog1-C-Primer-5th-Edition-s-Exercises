e04df99e9e3c6aa32b1a648677bec73001cd2660
守真 孫 <oscarsun72@hotmail.com>
2019-12-03 19:26:07 +0800

    temp

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -6,28 +6,12 @@
 
 #include<iostream>
 using namespace std;
-struct destination;	// represents what we are connecting to
-struct connection;           // information needed to use the connection
-connection connect(destination*); // open the connection
-void disconnect(connection);       // close the given connection
+
 int main() {	
+	unique_ptr<int>up(new int(12));
+	unique_ptr<int>up1=up;
 }
-void end_connection(connection* p) { disconnect(*p); }
-void f(destination & d /* other parameters */)
-	{
-		// get a connection; must remember to close it when done
-		connection c = connect(&d);
-		// use the connection
-		// if we forget to call disconnect before exiting f, there will be no way to close c
-	}
-void f(destination& d /* other parameters */)
-{
-	connection c = connect(&d);
-	//shared_ptr<connection> p(&c, end_connection);
-	shared_ptr<connection> p(&c, [c]{disconnect(c); });
-	// use the connection
-	// when f exits, even if by an exception, the connection will be properly closed
-}
+
 
 
 
