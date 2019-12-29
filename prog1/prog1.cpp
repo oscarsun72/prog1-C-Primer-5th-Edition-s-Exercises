7490d7e51852869ad9f3416b044996687dade2fc
守真 孫 <oscarsun72@hotmail.com>
2019-12-29 18:50:10 +0800

    OK

--- prog1/prog1.cpp
+++ prog1/prog1.cpp
@@ -8,8 +8,6 @@
 using namespace std;
 
 
-auto p = new int(1);
-auto sp = make_shared<int>(1);
 void process(shared_ptr<int> ptr)
 {
 	//cout << sp.use_count() << endl;
@@ -17,12 +15,21 @@ void process(shared_ptr<int> ptr)
 } // ptr超疇出範疇，並被摧毁了
 
 int main() {
+auto p = new int(1);
+auto sp = make_shared<int>(1);
 	//cout << sp.use_count() << endl;
-	process(sp);//legal，sp參考計數（reference count）會加1，proecss結束後減1。然而如練習12.10，若process內的區域智慧指標結束，要注意防止sp成為懸置指標（dangling pointer）
+	process(sp);//(a)legal，sp參考計數（reference count）會加1，proecss結束後減1。然而
+	//如練習12.10，若process內的區域智慧指標結束，要注意防止sp成為懸置指標（dangling pointer）
+	// sp並非區域的，所以不會隨出process範疇而被摧毀。且process內的區域智慧指標雖在process結束後摧毀，但與sp指向同一個記憶體位置，該位置物件仍有sp指向它，故雖然proecss區域的智慧指標摧毀了，也不會釋放該物件之記憶體。
 	//cout << sp.use_count() << endl;
-	cout << *sp << endl;
-	process(shared_ptr<int>(p)); //和（a）類似。這裡是將p所指向的物件操縱
-	cout << *p << endl;//p所指之記憶體已被釋放，p成了懸置指標（dangling pointer)
+	cout << "*sp=" << *sp << endl;	
+	cout << "*p=" << *p << endl;//p所指之記憶體已被釋放，p成了懸置指標（dangling pointer)
+
+	//(b)process(new int());//illegal，因為型別不合，不具隱含轉型。new回傳的是一個普通指標、內建指標，而不是智慧指標shared_ptr
+	//(c)process(p); //illegal，同前
+	//(d)process(shared_ptr<int>(p)); //和（a）類似。這裡是將p所指向的物件操縱權交給了process內區域智慧指標，p，就合該無法再對其物件作操作了，p當設為空指標，以免淪為懸置指標。參見Table 12.3. Other Ways to Define and Change shared_ptrs 表12.3 :定義和變更shared_ptr的其他方式：shared_ptr<T>p(q)
+	//此例中shared_ptr乃區域性的，故會被銷毀，而使p無效，成為懸置指標。
+
 }
 
 
