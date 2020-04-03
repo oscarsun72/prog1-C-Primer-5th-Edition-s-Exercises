680ac3b7597aa412b389dfb5d2c3de23d7159831
守真 孫 <oscarsun72@hotmail.com>
2020-04-04 03:20:51 +0800

    OK

--- prog1/HasPtr.h
+++ prog1/HasPtr.h
@@ -1,12 +1,13 @@
 ﻿#ifndef HASPTR_H
 #define HASPTR_H
 #include<string>
+#include<iostream>
 class HasPtr
 {
     friend void swap(HasPtr&, HasPtr&);
 public:
     HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0) {}
-    //每個HasPtr物件都有一個ps指標指向的那個string的副本
+    //拷貝建構器：每個HasPtr物件都有一個ps指標指向的那個string的副本
     HasPtr(const HasPtr& p) : ps(new std::string(*p.ps)), i(p.i) {}
     HasPtr& operator=(HasPtr);
     ~HasPtr() { delete ps; }
@@ -19,6 +20,7 @@ inline void swap(HasPtr&lhs,HasPtr&rhs){//頁517
     using std::swap;
     swap(lhs.ps, rhs.ps);
     swap(lhs.i, rhs.i);
+    std::cout << "swap here！感恩感恩 南無阿彌陀佛" << std::endl;
 }
 inline HasPtr& HasPtr::operator=(HasPtr rhs) {//頁518
     swap(rhs, *this);
